b9eb468ff44a2d6a9f298f1b10f3178d7bb7732e
Bogdan <lazavicbagdan@gmail.com>
2023-01-28 17:53:57 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import random
+
+
 class MyClass:
     # print("Hello World")
-    def __init__(self):
-        self.height = 180
+    how = 0
+
+    def __init__(self, height):
+        self.height = height
         print(self)
+        MyClass.how += 1
+
+    def grow2(self, lis=None):
+        if lis is None:
+            lis = []
+        self.mid = 0
+        for i in range(len(lis)):
+            self.mid += lis[i]
+        self.mid = self.mid / len(lis)
 
 
-var = MyClass
-print(var)
+lst = []
+for i in range(random.randint(5, 10)):
+    hei = random.randint(140, 180)
+    lst.append(MyClass(height=hei))
+    print(lst[i])
+print(MyClass.how)
+hi = MyClass.grow2(lis=lst)
+print(hi)
